b60b102b4deb2003e9cf2f0117d72558f88c09d6
Juan Carlos Arocha <09-10055@ldc.usb.ve>
2015-02-17 16:19:28 +0000

    Fixing to needed values of database, server, user, etc

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ foreach($restaurants as $res) {
 
 RestaurantInfo::stopRendering(); 
 
-var_dump($_ENV); 
 mysql_close(); 
 
 ?>
--- restaurant.php
+++ restaurant.php
@@ -1,10 +1,10 @@
 <?php 
 //Tables names
-define("RESTAURANTS", "restaurant");
-define("DISHES", "dish");
+define("RESTAURANTS", "0910336_restaurant");
+define("DISHES", "0910336_dish");
 
 // Some variables
-define("SERVER"  , $_SERVER['SERVER_ADDR']);
+define("SERVER"  , '127.0.0.1');
 define("USERNAME", '0910336');
 define("PASSWORD", '0910336');
 define("DB_NAME", 'ra');
