5afdc87492a36e12f4332072e996fd6782538f50
origds <origds@gmail.com>
2015-02-17 22:24:22 -0430

    testing stars only 3

--- arel/logic.js
+++ arel/logic.js
@@ -44,7 +44,7 @@ function handleCustomPoiEvent(obj, type, param)
         $('#star1').show();
         $('#star2').show();
         $('#star3').show();
-        $('#info .score').html("Calificacion: " + obj.getParameter("avg_score"));
+        //$('#info .score').html("Calificacion: " + obj.getParameter("avg_score"));
         if (($('#star6').is(':visible')) && ($('#star7').is(':visible')) && ($('#star8').is(':visible'))) {
             rankRestaurant(obj.getParameter("restaurant_id"),3);
             $('#info .score').html("Calificacion: " + obj.getParameter("avg_score"));
