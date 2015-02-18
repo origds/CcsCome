faf7caf2d7daaaf8874eb7ab7b926949b69619a3
origds <origds@gmail.com>
2015-02-17 22:43:06 -0430

    test ranking

--- arel/logic.js
+++ arel/logic.js
@@ -42,8 +42,15 @@ function handleCustomPoiEvent(obj, type, param)
         $('#info .buttons').html("<div class=\"button\" onclick=\"arel.Media.openWebsite('" + obj.getParameter("url") + "')\">" + obj.getParameter("url") + "</div>");
         $('#info').show();
         $('#star1').show();
+        $('#star1').html("<img style=\"width:20px;height:20px\" width=\"50\" heigh=\"50\" border=\"0\" src=\"../imagenes/blackstar.png\" ontouchstart=\"$('#star1').hide(); $('#star6').show();\">");
+        $('#star6').html("<img style=\"width:20px;height:20px\" width=\"50\" heigh=\"50\" border=\"0\" src=\"../imagenes/star.png\">");
         $('#star2').show();
+        $('#star2').html("<img style=\"width:20px;height:20px\" width=\"50\" heigh=\"50\" border=\"0\" src=\"../imagenes/blackstar.png\" ontouchstart=\"$('#star2').hide(); $('#star7').show();\">");
+        $('#star7').html("<img style=\"width:20px;height:20px\" width=\"50\" heigh=\"50\" border=\"0\" src=\"../imagenes/star.png\">");
         $('#star3').show();
+        $('#star3').html("<img style=\"width:20px;height:20px\" width=\"50\" heigh=\"50\" border=\"0\" src=\"../imagenes/blackstar.png\" ontouchstart=\"$('#star3').hide(); $('#star8').show();\">");
+        $('#star8').html("<img style=\"width:20px;height:20px\" width=\"50\" heigh=\"50\" border=\"0\" src=\"../imagenes/star.png\">");
+        
         $('#info .score').html("Calificacion: " + obj.getParameter("avg_score").toString());
         if (($('#star6').is(':visible')) && ($('#star7').is(':visible')) && ($('#star8').is(':visible'))) {
             rankRestaurant(obj.getParameter("restaurant_id"),3);
